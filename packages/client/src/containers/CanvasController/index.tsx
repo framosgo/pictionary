e8015930f28794851628a8d1b92b4e08e7ca6b82
Marisa Robayo Moreno <marisarobayo@gmail.com>
2021-03-06 11:19:39 +0100

    WIP: creating canvas

---
+++ packages/client/src/containers/CanvasController/index.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import Canvas from '../../components/Canvas';
+
+
+interface Props {
+
+}
+
+export const CanvasController: React.FC<Props> = (props) => {
+
+    
+
+    return (
+        <Canvas/>
+    )
+}
